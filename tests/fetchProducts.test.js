c6257b457b594abd3c66ced72029f4f529c42f5a
Pedro Roberto Bezerra De Medeiros <pedropwks@gmail.com>
2022-10-11 15:36:18 -0300

    Adiciona requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,28 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Testando se é uma função', () => {
+    expect(typeof fetchProducts).toBe('function')
+  })
+
+  test('Testando se o fetch foi chamado', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  })
+
+  test('Testando o retorno de fetch', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(url);
+  })
+
+  test('Testando o retorno da função', async () => {
+    const chamada = await fetchProducts('computador');
+    expect(chamada).toEqual(computadorSearch);
+  })
+
+  test('Testando o retorno da função sem argumentos', async () => {
+    const chamada = await fetchProducts();
+    expect(chamada).toEqual(new Error('You must provide an url'));
+  })
 });
