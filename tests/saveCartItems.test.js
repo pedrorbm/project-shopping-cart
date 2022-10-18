7595d8c60535c03d2f1217e113ed32402aea96bd
Pedro Roberto Bezerra De Medeiros <pedropwks@gmail.com>
2022-10-18 15:44:11 -0300

    Adiciona requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,11 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', () => {
+    expect(saveCartItems('cartItem')).localStorageSimulator('setItem')
+  })
+
+  test(`Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave 'cartItems' e o segundo sendo o valor passado como argumento para saveCartItems`, () => {
+    expect(saveCartItems('cartItem')).localStorageSimulator('setItem').toEqual({cartItem: ['ID: MLB1937076326 | TITLE: Pc Computador Cpu Core I5 650 + Ssd 240gb, 8gb Memória Ram | PRICE: $1062.5']});
+  })
 });
